88426f314aa978723553454dc8905aa06a731ece
Joshua <joshaw66@gmail.com>
2020-03-05 10:47:35 -0700

    creating file structures/creating Card and Data files

---
+++ src/components/Data.js
@@ -0,0 +1,32 @@
+import React, {useEffect, useState} from "react";
+import Card from "./Card";
+import axios from "axios";
+
+function Data() {
+
+let [Data, setData] = useState([])
+useEffect(() => {
+  axios
+    .get("https://api.nasa.gov/planetary/apod")
+    .then(response => {
+      console.log(response.data);
+      setData(response.data);
+    })
+    .catch(error => {
+      console.log("No Data can be returned!", error);
+    })
+}, []);
+return (
+  <div className="image"> 
+        <Card
+            key={Data.id}
+            title={Data.title}
+            explanation={Data.explanation}
+            date={Data.date}
+            img = {Data.url}
+        />
+  </div>
+);
+}
+
+export default Data;
